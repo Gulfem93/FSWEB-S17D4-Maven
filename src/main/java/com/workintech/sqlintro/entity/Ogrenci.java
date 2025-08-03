04d13af05af7209db23a5936fb0aef83382ec3cd
Gülfem IŞIK <35203615+Gulfem93@users.noreply.github.com>
2025-08-03 06:30:00 +0300

    derlendi

--- src/main/java/com/workintech/sqlintro/entity/Ogrenci.java
+++ src/main/java/com/workintech/sqlintro/entity/Ogrenci.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
@@ -14,11 +16,21 @@ public class Ogrenci {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long ogrno;
+
+    @Column(nullable = false)
     private String ad;
+
+    @Column(nullable = false)
     private String soyad;
+
+    @Column(nullable = false)
     private String cinsiyet;
+
+    @Column(nullable = false)
     private String sinif;
+
     private int puan;
+
     @Column(nullable = true)
-    private String dtarih;
+    private LocalDate dtarih;
 }
